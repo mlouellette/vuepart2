5fd4f8a0c9a94571a86717682e0f5c7e233397b3
mlouellette <112026857+mlouellette@users.noreply.github.com>
2023-09-10 19:52:08 -0400

    init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<script>
+  import flash from "@/mixins/flash";
+
+  export default {
+    mixins: [flash]
+  };
+
+</script>
+
+<template>
+  <div>
+    <p>
+      <button @click="flash('It works on the about page')">Click Me</button>
+    </p>
+
+  </div>
+</template>
+
